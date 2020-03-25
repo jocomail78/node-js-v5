2ba108fa8892d62f5b834e99a879f2a7d827313b
Peter Jozsef <j.peter@camelweb.com>
2020-03-25 18:51:14 +0200

    Code changes + updated eslint rules

--- src/App.js
+++ src/App.js
@@ -1,15 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
-// const Pet = props => {
-const Pet = ({ name, animal, breed }) => {
-  //destructuring way
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
