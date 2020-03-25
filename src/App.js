577c4d658de6f04e6854289bb816132840c71216
Peter Jozsef <j.peter@camelweb.com>
2020-03-25 18:27:08 +0200

    Include React + DOM, change index inclusion

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
 // const Pet = props => {
 const Pet = ({ name, animal, breed }) => {
   //destructuring way
@@ -29,4 +32,4 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(React.createElement(App), document.getElementById("root"));
