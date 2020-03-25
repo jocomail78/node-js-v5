6f7061edb951a6bec1365067c1c49f1e8863e439
Peter Jozsef <j.peter@camelweb.com>
2020-03-25 17:49:45 +0200

    Project baseline config

---
+++ src/App.js
@@ -0,0 +1,32 @@
+// const Pet = props => {
+const Pet = ({ name, animal, breed }) => {
+  //destructuring way
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", { id: "something-important" }, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cocktaiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
