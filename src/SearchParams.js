3f3f47bc7940bbc7df00a3b96603cd616780cff0
Peter Jozsef <j.peter@camelweb.com>
2020-03-28 11:26:21 +0200

    Working hooks + comment

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SeachParams = () => {
@@ -8,7 +8,24 @@ const SeachParams = () => {
   //   const [breed, setBreed] = useState("");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreeedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreeedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  //adding an effect, which will be scheduled to run after rendering the DOM content
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error);
+    setBreeds([]);
+    setBreed("");
+
+    //this is an API call.
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); //defining the effect dependencies, basically the parameters or values,
+  //which, in case if they are changed, should schedule this effect to run again. If you want it to run once and
+  //never again, just add an empty array as the dependency array. If you want to run it every time it updates,
+  //then remove the empty array, which will mean that it depends on everything. Basically that will be an infinite loop.
+  //Which is not good at all.
 
   return (
     <div className="search-params">
