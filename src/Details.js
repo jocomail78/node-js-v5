c4ca24f856fe4d3e8a2dffd50e9ce8a8785fc84c
Peter Jozsef <j.peter@camelweb.com>
2020-03-29 17:04:47 +0300

    Details page showing real details

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,44 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = () => {
-  return <h1>hi!</h1>;
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
+  componentDidMount() {
+    //runs at first startup, and won't run again
+    //good for loading ajax
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+    const { animal, breed, location, description, name } = this.state;
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
